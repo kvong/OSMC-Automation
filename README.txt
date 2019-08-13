4a81d877432dd39de1828841f733693e341fc7ac
osmc <vongk7@gmail.com>
2019-08-13 16:23:27 +0000

    Able to switch between shows

--- README.txt
+++ README.txt
@@ -1,11 +1,11 @@
 Description: Automatically play and turn off background tv show. ( Great for people who sleeps with the tv on )
 
 Prerequisite:
-	bookmark.dat ( contain the index of the last episode played by osmc )
-	playlist.dat ( contain the names of all the video files in a list)
+	showname_bookmark.dat ( contain the index of the last episode played by osmc )
+	showname_episodelist.dat ( contain the names of all the video files in a list)
 
 Features:
 	Python script will take the bookmark number and read that number of lines in playlist.dat.
-	From that position it will create a list of the next 10 episode ( if reach the end, loop over)
+	From that position it will create a list of the next 9 episode ( if reach the end, loop over)
 	
 	Shutdown in 3 hours.
--- autoexec.py
+++ autoexec.py
@@ -1,45 +1,68 @@
+# Import xbmc library to control osmc player
 import xbmc
-
 import time
 
 # Author: Khanh Vong
+# Description: Tv show automation
 
-# Directory where out videos are located
-d = "/media/0C6B-AD88/HIMYM/" 
+# Pick show to play "HIMYM" or "FRIENDS"
+#show = "FRIENDS"
+show = "HIMYM"
+
+# Directory where our videos are located
+d = "/media/ElementDrive/" + show + "/" 
+
+# Change episode list depending on show
+if show == "HIMYM":
+    episodes = 208
+elif show == "FRIENDS":
+    episodes = 234
+else:
+    episodes = 0
 
 # Open to read number from bookmark.dat as an integer
-f = open("/home/osmc/.kodi/userdata/bookmark.dat", "r")
+f = open("/home/osmc/.kodi/userdata/Automation.dat/" + show + "_bookmark.dat", "r")
 bookmark = int(f.readline())
 # Use modulus so that playlist will start over when we reach the end
-bookmark = ((bookmark + 9) % 208) + 1
-f.closed
+start = bookmark
+bookmark = ((bookmark + 8) % episodes) + 1
+f.close()
+
+# To know which episode to play
+# - Pick an episode and subtract 9 from it
+
 
 # Open to update bookmark.dat for time we power on
-f = open("/home/osmc/.kodi/userdata/bookmark.dat", "w")
-f.write(str(bookmark))
-f.closed
+f = open("/home/osmc/.kodi/userdata/Automation.dat/" + show + "_bookmark.dat", "w+")
+f.write(str(bookmark)) #Note: subtract 4 to handle tv double powered on bug *works better with even playlist
+f.close()
 
-# Open playlist.dat for reading
-f = open("/home/osmc/.kodi/userdata/playlist.dat", "r")
-# Loop through every video individual video in playlist.dat
-for i in range(208):
+# Open episodelist.dat for reading
+f = open("/home/osmc/.kodi/userdata/Automation.dat/" + show + "_episodelist.dat", "r")
+# Loop through every video individual video in xxxxx_episodelist.dat
+for i in range(episodes):
     # Once we find where the book mark is loop, start adding video to a list
     episode = f.readline().strip('\n')
-    if i + 1 == bookmark:
+    if i + 1 == start :
         # Read line from playlist.dat removing \n at the end
         episode = d + episode
         episodelist = [ episode ]
-        for j in range(9):
+        fw = open("/home/osmc/.kodi/userdata/Automation.dat/currentplaylist.dat", "w+")
+        fw.write(str(episode) + '\n')
+        for j in range(8):
             episode = f.readline().strip('\n')
             episode = d + episode
             episodelist.append( episode )
+            fw.write(str(episode) + '\n')
+        fw.close()
         break
-f.closed
+f.close()
+
 
 # Get osmc current playlist and clear it
 playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
 playlist.clear()
-
+    
 # Add list to playlist
 for k in range(len(episodelist)):
     playlist.add(str(episodelist[k]))
@@ -48,6 +71,7 @@ for k in range(len(episodelist)):
 xbmc.Player().play(playlist)
 
 
-# Set timer for shutdown ( 3 hours )
-xbmc.sleep(10800000)
+# Set timer for shutdown ( 200 minutes )
+# converted to seconds
+time.sleep(12000)
 xbmc.shutdown()
