773ef996233825133bbf06249d9006a909552c2b
Blank <vongk7@gmail.com>
2020-04-06 11:54:58 -0500

    Build script updated

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo apt-get -y update
 cp -r ./Automation.dat ~/.kodi/userdata/
 cat ./autoexec.py > ~/.kodi/userdata/autoexec.py
-sudo apt install -y vim python-numpy
+sudo apt install -y vim python-numpy build-essential
 git clone https://github.com/FFmpeg/FFmpeg.git ~/ffmpeg
 cd ~/ffmpeg/
 ./configure
