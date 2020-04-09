4c6c9d115e7a4991b7bf86fe8ba4d6eec70dfb4f
blank <vongk7@gmail.com>
2020-04-09 15:27:48 -0500

    Install script updated

--- install.sh
+++ install.sh
@@ -1,18 +1,25 @@
-echo Updating repos...
-sudo apt-get -y update
-echo Copying over Automation.dat...
-cp -r ./Automation.dat ~/.kodi/userdata/
-echo Copying over autoexec.py...
-cat ./autoexec.py > ~/.kodi/userdata/autoexec.py
-echo Updating .bashrc...
-cat ./alias >> ~/.bashrc
-echo Installing prerequisites...
-sudo apt install -y vim python-numpy build-essential
-echo Creating FFmpeg...
-git clone https://github.com/FFmpeg/FFmpeg.git ~/ffmpeg
-cd ~/ffmpeg/
-./configure
-make
-sudo make install
-echo OSMC-Automation completed. Rebooting in 5 seconds.
-sleep 5s
+echo "Install script should be executed in the '/home/osmc/OSMC-Automation/' directory. Do you wish to continue? [y/n]"
+read choice
+
+if [ choice == 'y' ] || [ choice == 'Y' ] || [ choice == 'yes' ] ||| [ choice == 'YES' ] 
+then
+    echo Updating repos...
+    sudo apt-get -y update
+    echo Copying over Automation.dat...
+    cp -r ./Automation.dat ~/.kodi/userdata/
+    echo Copying over autoexec.py...
+    cat ./autoexec.py > ~/.kodi/userdata/autoexec.py
+    echo Updating .bashrc...
+    cat ./alias >> ~/.bashrc
+    echo Installing prerequisites...
+    sudo apt install -y vim python-numpy build-essential
+    echo Creating FFmpeg...
+    git clone https://github.com/FFmpeg/FFmpeg.git ~/ffmpeg
+    cd ~/ffmpeg/
+    ./configure
+    make
+    sudo make install
+    echo OSMC-Automation completed. Rebooting in 5 seconds.
+    sleep 5s
+    reboot
+fi
