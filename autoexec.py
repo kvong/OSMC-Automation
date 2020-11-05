adaf454a8f4b0bf848339db39b2f54a81e9ed648
Blank <vongk7@gmail.com>
2020-11-04 20:44:08 -0600

    Added episode history logging

--- autoexec.py
+++ autoexec.py
@@ -5,6 +5,7 @@
 
 # Import xbmc library to control osmc player
 from scheduler import Scheduler # Newly Added
+
 import xbmc     # REMOVE FOR TEST
 import time
 import datetime
@@ -16,7 +17,7 @@ randomize = 1
 sequential = 0
 
 # Set watch option
-watch_option = sequential
+watch_option = randomize
 
 # Pick show to play "HIMYM" or "FRIENDS"
 show = "DrakeAndJosh"
@@ -28,9 +29,9 @@ show = "test"
 hour = datetime.datetime.now().hour
 
 # Set size of playlist according to time of day 
-if hour > 8 and hour < 21:
+if hour > 5 and hour < 21:
     # During day time set long playlist
-    size = 18
+    size = 9
     show = "FRIENDS"
     show = "BigBang"
     #show = "test"      # UNCOMMENT FOR TEST
@@ -61,10 +62,9 @@ random_point = 0
 if ( watch_option == sequential ):
     # Watch with bookmarks
     # Open to read number from bookmark.dat as an integer
-    f = open("/home/osmc/.kodi/userdata/Automation.dat/" + show + "_bookmark.dat", "r")
-    bookmark = int(f.readline())
+    with open("/home/osmc/.kodi/userdata/Automation.dat/" + show + "_bookmark.dat", "r") as f:
+        bookmark = int(f.readline())
     start = bookmark
-    f.close()
 elif ( watch_option == randomize ):
     mem_filename = "/home/osmc/.kodi/userdata/Automation.dat/" + show + "_mem.dat"
     random_point, available, available_extend, block_start, block_stop, open_index = Scheduler.RandomFirstFit(mem_filename, episodes, size)
@@ -75,27 +75,25 @@ entire_episodelist = []
 current_playlist = []
 # Write to current playlist
 # Open playlist.dat for reading
-f = open("/home/osmc/.kodi/userdata/Automation.dat/" + show + "_episodelist.dat", "r")
+with open("/home/osmc/.kodi/userdata/Automation.dat/" + show + "_episodelist.dat", "r") as f:
 # Loop through every video individual video in xxxxx_episodelist.dat
-for i in range(episodes):
-    # Once we find where the book mark is loop, start adding video to a list
-    episode = f.readline().strip('\n')
-    if i + 1 == start :
-        current_playlist.append( episode )
-        # Read line from playlist.dat removing \n at the end
-        episode = d + episode
-        episodelist.append( episode )
-        fw = open("/home/osmc/.kodi/userdata/Automation.dat/currentplaylist.dat", "w+")
-        fw.write(str(episode) + '\n')
-        for j in range(size - 1):
-            episode = f.readline().strip('\n')
+    for i in range(episodes):
+        # Once we find where the book mark is loop, start adding video to a list
+        episode = f.readline().strip('\n')
+        if i + 1 == start :
             current_playlist.append( episode )
+            # Read line from playlist.dat removing \n at the end
             episode = d + episode
             episodelist.append( episode )
-            fw.write(str(episode) + '\n')
-        fw.close()
-    entire_episodelist.append( episode )
-f.close()
+            with open("/home/osmc/.kodi/userdata/Automation.dat/currentplaylist.dat", "w+") as fw:
+                fw.write(str(episode) + '\n')
+                for j in range(size - 1):
+                    episode = f.readline().strip('\n')
+                    current_playlist.append( episode )
+                    episode = d + episode
+                    episodelist.append( episode )
+                    fw.write(str(episode) + '\n')
+        entire_episodelist.append( episode )
 
 # Add padding at the end of entire_episodelist
 entire_episodelist.extend( entire_episodelist )
@@ -118,6 +116,7 @@ xbmc.Player().play(playlist)                       # REMOVE ON TEST
 import subprocess
 
 def get_duration(filename):
+    # Execute "ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 /media/ElementDrive/BigBang/S08E09.mp4
     result = subprocess.check_output(["/home/osmc/ffmpeg/ffprobe", "-v", "error", "-show_entries", 
             "format=duration", "-of", "default=noprint_wrappers=1:nokey=1",
             filename])
@@ -137,9 +136,8 @@ for k in range(size):
         bookmark = (bookmark % episodes) + 1
 
         ## Update bookmark
-        f = open("/home/osmc/.kodi/userdata/Automation.dat/" + show + "_bookmark.dat", "w+")
-        f.write(str(bookmark)) 
-        f.close()
+        with open("/home/osmc/.kodi/userdata/Automation.dat/" + show + "_bookmark.dat", "w+") as f:
+            f.write(str(bookmark)) 
     elif ( watch_option == randomize ):
         # Update episode list one at a time
         available_extend[random_point + k] = 1
@@ -147,17 +145,15 @@ for k in range(size):
         # If available list is empty, then the vector is all 0s
         if len(available) == 0:
             # Write new available vector to file
-            f = open(mem_filename, 'w')
-            print('Writing blocks %d' % random_point)
-            f.writelines([str(available_extend[i]) + '\n' for i in range(episodes)])
-            f.close()
+            with open(mem_filename, 'w') as f:
+                print('Writing blocks %d' % random_point)
+                f.writelines([str(available_extend[i]) + '\n' for i in range(episodes)])
         else:
             open_index_length = len(open_index)
             if open_index_length == 0:
-                f = open(mem_filename, 'w')
-                for j in range(episodes):
-                    f.write(str(available_extend[j]) + '\n')
-                f.close()
+                with open(mem_filename, 'w') as f:
+                    for j in range(episodes):
+                        f.write(str(available_extend[j]) + '\n')
                 print('Writing blocks %d' % (random_point + count))
                 count += 1
                 print(available_extend)
@@ -166,35 +162,37 @@ for k in range(size):
                     # Check that the block is valid for playing
                     if block_start[i] <= random_point and block_stop[i] > random_point:
                         # Writing a the block to 'mem'
-                        f = open(mem_filename, 'w')
-                        print('Writing blocks %d' % random_point)
-                        count += 1
-                        f.writelines([str(available_extend[j]) + '\n' for j in range(episodes)])
-                        print(available_extend)
-                        f.close()
+                        with open(mem_filename, 'w') as f:
+                            print('Writing blocks %d' % random_point)
+                            count += 1
+                            f.writelines([str(available_extend[j]) + '\n' for j in range(episodes)])
+                            print(available_extend)
                         break
 
     episode_duration = get_duration(episode_filename)       # REMOVE ON TEST
 
     # Create a summary file for quick viewing without looking into dat files
-    f_info = open('/home/osmc/.kodi/userdata/Automation.dat/summary.dat', 'w+')
-    f_info.write('Current show: %s\n' % (show))
-    f_info.write('Current episode: %d\n' % (start + k))
-    f_info.write('Current playlist:\n')
-    for i in range(len(current_playlist)):
-        if i == k:
-            f_info.write('> %d. %s\n' % ( i + 1, current_playlist[i]))
-        else:
-            f_info.write('  %d. %s\n' % ( i + 1, current_playlist[i]))
-    f_info.close()
+    with open('/home/osmc/.kodi/userdata/Automation.dat/summary.dat', 'w+') as f_info:
+        f_info.write('Current show: %s\n' % (show))
+        f_info.write('Current episode: %d\n' % (start + k))
+        f_info.write('Current playlist:\n')
+        for i in range(len(current_playlist)):
+            if i == k:
+                f_info.write('> %d. %s\n' % ( i + 1, current_playlist[i]))
+            else:
+                f_info.write('  %d. %s\n' % ( i + 1, current_playlist[i]))
+
+        # Create a history log
+        with open("/home/osmc/.kodi/userdata/Automation.dat/" + show + "_hist.dat", "a") as f_hist:
+            f_hist.write('%d\n' % (start))
+
     # Sleep until episode ends         # REMOVE ON TEST 
     time.sleep( episode_duration )     # REMOVE ON TEST 
-
 time.sleep(60*3)                       # REMOVE ON TEST
 
-f_info = open('/home/osmc/.kodi/userdata/Automation.dat/summary.dat', 'w+')
-f_info.write('No show playing at the moment.\n')
-f_info.close()
+with open('/home/osmc/.kodi/userdata/Automation.dat/summary.dat', 'w+') as f_info:
+    f_info.write('No show playing at the moment.\n')
+    f_info.close()
 
-# Log playlist at the end of playthrough    # REMOVE ON TEST
+## Log playlist at the end of playthrough    # REMOVE ON TEST
 xbmc.shutdown()                             # REMOVE ON TEST 
